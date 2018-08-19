5ca1290cdb8a4b262888aa1fb0e1560d069f1495
Spitfire <twisterintwist@gmail.com>
2018-08-19 13:45:28 +0530

    Adding lazy loaded module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'lazy-load',
+    loadChildren: './lazy-load/lazy-load.module#LazyLoadModule'
   }
 ];
 
